3aaeb2eb70b45a63832fc1c19abd553e301b90f5
Anshul Jain <anshulj634@gmail.com>
2026-07-15 16:21:33 +0530

    Time: 165 ms (74.77%), Space: 132.7 MB (9.03%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int countPrimes(int n) {
         if(n==0||n==1) return 0;
-        vector<int> arr(n+1,1);
+        vector<int> arr(n,1);
         for(int i=2;i<=(n-1)/i;i++){
             if(arr[i]==1){
-                for(int j=i*i;j<=n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     arr[j]=0;
                 }
             }
