91966f5834dba674f715d83e70d3e935fe510445
Anshul Jain <anshulj634@gmail.com>
2026-08-01 00:09:26 +0530

    Time: 0 ms (100%), Space: 10.4 MB (49.4%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -23,16 +23,23 @@ public:
         }
         return true;
     }
-    void solve(int row, vector<string>&board, vector<vector<string>>&ans, int n){
+    void solve(int row, vector<string>&board, vector<vector<string>>&ans, vector<int>&topRow,
+                                vector<int>&leftDiagonal,vector<int>&rightDiagonal, int n){
         if(row==n){
             ans.push_back(board);
             return;
         }
         for(int col=0;col<n;col++){
-            if(isSafe(row, col, board,n)){
+            if(topRow[col]==0&&leftDiagonal[n-1+col-row]==0&&rightDiagonal[row+col]==0){
                 board[row][col]='Q';
-                solve(row+1, board, ans, n);
+                topRow[col]=1;
+                leftDiagonal[n-1+col-row]=1;
+                rightDiagonal[row+col]=1;
+                solve(row+1, board, ans, topRow, leftDiagonal, rightDiagonal, n);
                 board[row][col]='.';
+                topRow[col]=0;
+                leftDiagonal[n-1+col-row]=0;
+                rightDiagonal[row+col]=0;
             }
         }
     }
@@ -43,7 +50,8 @@ public:
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        solve(0,board,ans, n);
+        vector<int> topRow(n,0), leftDiagonal(2*n-1,0), rightDiagonal(2*n-1,0);
+        solve(0,board,ans,topRow, leftDiagonal, rightDiagonal,n);
         return ans;
     }
 };
