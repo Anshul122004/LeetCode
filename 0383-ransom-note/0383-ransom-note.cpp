2946f751a0105b880c20902c0f7b470736ee5621
Anshul Jain <anshulj634@gmail.com>
2026-08-18 12:27:00 +0530

    Time: 3 ms (52.53%), Space: 11.9 MB (30.25%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(char ch : magazine){
+            mp[ch]++;
+        }
+        for(char ch: ransomNote){
+            if(mp.find(ch)!=mp.end()){
+                if(mp[ch]<1){
+                    return false;
+                }
+                mp[ch]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
